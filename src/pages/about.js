8eeb76183b62b5e0fcd6060e56f94cd0f4d79266
YathaarthSuri <yathaarthsuri@gmail.com>
2023-08-08 21:53:14 +0530

    Completed experience section

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,7 @@ import React, { useEffect, useRef } from 'react';
 import profilePic from '../../public/images/profile/profilePhoto.jpg';
 import { useInView, useMotionValue, useSpring } from 'framer-motion';
 import Skills from '@/components/Skills';
+import Experience from '@/components/Experience';
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -98,6 +99,8 @@ const about = () => {
           </div>
 
           <Skills />
+
+          <Experience />
         </Layout>
       </main>
     </>
